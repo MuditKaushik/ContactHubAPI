24ff2bccbf7c928df27edccfb94e1b0d57d17663
muditm18@gmail.com <muditm18@gmail.com>
2017-03-06 09:44:14 +0530

    project added.

---
+++ ContactHubAPI/ContactHubAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
+
+namespace ContactHubAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            EnableCORS(config);
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            //Json Format
+            SetJsonFormatter(config);
+            //Routing
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        public static void SetJsonFormatter(HttpConfiguration config)
+        {
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        public static void EnableCORS(HttpConfiguration config)
+        {
+            var Cors = new EnableCorsAttribute(origins:"*",headers:"*",methods:"*",exposedHeaders:"*");
+            config.EnableCors(Cors);
+        }
+    }
+}
---
+++ ContactHubAPI/ContactHubAPI/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactHubAPI.Models.User;
+
+namespace ContactHubAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public IHttpActionResult Login(LoginUserViewModel model)
+        {
+            return Ok(model);
+        }
+    }
+}
